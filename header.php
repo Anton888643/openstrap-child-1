387de511fa128cd960bd9e51cb43f6a9b174518b
Anton888643 <anton888@ukr.net>
2019-03-29 16:51:24 +0200

    Update header.php
    відключення слайдера
    
    Co-Authored-By: Anton <anton888643@users.noreply.github.com>

--- header.php
+++ header.php
@@ -241,17 +241,17 @@
 
     <div class="container" id="main-container">
     
-    <div class="col-md-12" >
+ <!--  <div class="col-md-12" >
     <?php
-        if ( is_front_page() ){
-            if (!wp_is_mobile()){
-                echo do_shortcode('[crellyslider alias="main_page_slider"]');
-            } else {
-                echo do_shortcode('[crellyslider alias="main_page_slider_mob"]');
+       # if ( is_front_page() ){
+        #    if (!wp_is_mobile()){
+          #      echo do_shortcode('[crellyslider alias="main_page_slider"]');
+          #  } else {
+          #      echo do_shortcode('[crellyslider alias="main_page_slider_mob"]');
             }
         }
     ?>
-    </div>
+    </div> -->
     <p>
 
 	<div class="row" id="main-row">
